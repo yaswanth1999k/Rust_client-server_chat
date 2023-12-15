d0cb53ed3c0983d5226f95332dafb2158f8da857
yaswanth1999k <153561576+yaswanth1999k@users.noreply.github.com>
2023-12-14 19:07:22 -0500

    Update main.rs
    update to get an user name and get the current timestamp

--- chat/server/src/main.rs
+++ chat/server/src/main.rs
@@ -2,26 +2,28 @@ use std::io::{ErrorKind, Read, Write};
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
+use std::time::Duration;
 
 const LOCAL: &str = "127.0.0.1:6000";
 const MSG_SIZE: usize = 32;
 
 fn sleep() {
-    thread::sleep(::std::time::Duration::from_millis(100));
+    thread::sleep(Duration::from_millis(100));
 }
 
 fn main() {
     let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
-    server.set_nonblocking(true).expect("failed to initialize non-blocking");
+    server.set_nonblocking(true).expect("Failed to initialize non-blocking");
 
     let mut clients = vec![];
     let (tx, rx) = mpsc::channel::<String>();
+
     loop {
         if let Ok((mut socket, addr)) = server.accept() {
             println!("Client {} connected", addr);
 
             let tx = tx.clone();
-            clients.push(socket.try_clone().expect("failed to clone client"));
+            clients.push(socket.try_clone().expect("Failed to clone client"));
 
             thread::spawn(move || loop {
                 let mut buff = vec![0; MSG_SIZE];
@@ -31,12 +33,17 @@ fn main() {
                         let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
                         let msg = String::from_utf8(msg).expect("Invalid utf8 message");
 
-                        println!("{}: {:?}", addr, msg);
-                        tx.send(msg).expect("failed to send msg to rx");
-                    }, 
-                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                        // Print the received message directly
+                        println!("{} [{}]: {}", addr, chrono::Local::now(), msg);
+
+                        tx.send(msg.clone()).expect("Failed to send msg to rx");
+                    },
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => {
+                        // Non-blocking operation not ready yet, sleep for a short duration
+                        sleep();
+                    },
                     Err(_) => {
-                        println!("closing connection with: {}", addr);
+                        println!("Closing connection with: {}", addr);
                         break;
                     }
                 }
